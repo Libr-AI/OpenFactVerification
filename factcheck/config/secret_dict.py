d01df0940aaf3f9ca120ec483404dc52b70ece29
haonan-li <nathan.8270.n@gmail.com>
2024-04-04 17:13:34 +0400

    format

--- factcheck/config/secret_dict.py
+++ factcheck/config/secret_dict.py
@@ -1,15 +1,11 @@
 import os
 
-serper_dict = {
-    "default_key" : None
-}
-openai_dict = {
-    "default_key" : None
-}
-anthropic_dict = {
-    "default_key": None
-}
+serper_dict = {"default_key": None}
+openai_dict = {"default_key": None}
+anthropic_dict = {"default_key": None}
 
 serper_dict["key"] = os.environ.get("SERPER_API_KEY", serper_dict["default_key"])
 openai_dict["key"] = os.environ.get("OPENAI_API_KEY", openai_dict["default_key"])
-anthropic_dict["key"] = os.environ.get("ANTHROPIC_API_KEY", anthropic_dict["default_key"])
+anthropic_dict["key"] = os.environ.get(
+    "ANTHROPIC_API_KEY", anthropic_dict["default_key"]
+)
