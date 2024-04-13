e730ed986a72e516c6ed7b9f17d7cc2e4d6a02b4
Xudong Han <xudongh1@student.unimelb.edu.au>
2024-04-14 01:37:27 +0800

    rename client mapper

--- factcheck/utils/llmclient/__init__.py
+++ factcheck/utils/llmclient/__init__.py
@@ -2,7 +2,7 @@ from factcheck.utils.llmclient.gpt_client import GPTClient
 from factcheck.utils.llmclient.claude_client import ClaudeClient
 
 
-def model2client(model_name: str):
+def client_mapper(model_name: str):
     # router for model to client
     if model_name.startswith("gpt"):
         return GPTClient
